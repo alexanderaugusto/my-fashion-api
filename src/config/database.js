8d466dbd35c36e23124673d68d651b4cb0dba662
Alexander Augusto <alexaasf_10@hotmail.com>
2020-06-02 09:11:09 -0300

    new changes

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,12 @@
 // Fazendo a conexão com o banco de dados SQL
+const type = "local"
+
 module.exports = {
   dialect: "mysql",
-  host: "centralmodas.cofdf77nzr6v.us-east-1.rds.amazonaws.com",
-  username: "admin",
-  password: "myData33Connect",
-  database: "centralmodas",
+  host: type === "local" ? "localhost" : "centralmodas.cofdf77nzr6v.us-east-1.rds.amazonaws.com",
+  username: type === "local" ? "root" : "admin",
+  password: type === "local" ? "root" : "myData33Connect",
+  database: type === "local" ? "central_modas" : "centralmodas",
   define: {
     timestamps: true, // created at - updated at
     underscored: true, // snake case (central_modas)
