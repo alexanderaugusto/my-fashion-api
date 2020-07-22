6ab92062276b2282973ba76ce12e59b574d537f1
Alexander Augusto <alexaasf_10@hotmail.com>
2020-07-22 00:58:55 -0300

    new changes

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,12 @@
 // Fazendo a conexão com o banco de dados SQL
-const type = "local"
+const type = process.env.DATABASE_TYPE
 
 module.exports = {
-  dialect: "mysql",
-  host: type === "local" ? "localhost" : "centralmodas.cofdf77nzr6v.us-east-1.rds.amazonaws.com",
-  username: type === "local" ? "root" : "admin",
-  password: type === "local" ? "root" : "myData33Connect",
-  database: type === "local" ? "central_modas" : "centralmodas",
+  dialect: "postgres",
+  host: type === "local" ? "localhost" : "ruby.db.elephantsql.com",
+  username: type === "local" ? "root" : "moiycdmj",
+  password: type === "local" ? "root" : "itSu9SHzdOMLKfJ7Ry0CAk2dN8D1qdKe",
+  database: type === "local" ? "central_modas" : "moiycdmj",
   define: {
     timestamps: true, // created at - updated at
     underscored: true, // snake case (central_modas)
